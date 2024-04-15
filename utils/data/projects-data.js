e3af69871f03ae9099385d4a46dbe714803103d5
CHENDJOU CHENDJOU HONORE <chenxhenor@gmail.com>
2024-04-15 11:57:48 +0000

    project

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -26,8 +26,8 @@ export const projectsData = [
     },
     {
         id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
+        name: 'Innovschool',
+        description: 'My team built an school management app using React as Front and Express springsboot for backend, with a Mongodb database. We used Express, Typescript, OpenAI, Replicate, Stripe, and pr to develop . We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
         tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
         code: '',
         role: 'Full Stack Developer',
